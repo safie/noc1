0ef581770b8d35f8ed13fd79c193601574cbf5f3
Safie Misri <safie19@gmail.com>
2022-10-30 00:14:48 +0800

    update

--- config/app.php
+++ config/app.php
@@ -195,8 +195,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\DomPDF\ServiceProvider::class,
-
     ],
 
     /*
@@ -212,7 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
+
     ])->toArray(),
 
 ];
