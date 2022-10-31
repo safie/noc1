aec60bcd79ca2d32c7bd5b77c1cf917e6ecd639d
safie <epu.bpm@gmail.com>
2022-10-31 09:05:37 +0800

    update

--- resources/views/page/pdf/noc_pdf.blade.php
+++ resources/views/page/pdf/noc_pdf.blade.php
@@ -1,139 +1,33 @@
 @extends('layouts.appPdf')
 
 @section('css')
-<style>
-    .page-break {
-        page-break-after: always;
-    }
+    <style>
+        .grid-container {
+            display: grid;
+        }
+
+        .header {
+            grid-column: 1 / span 2;
+        }
+
+        .page-break {
+            page-break-after: always;
+        }
     </style>
 @endsection
 
 @section('content')
-<div class="row gx-4">
-    <div class="col-lg-5">
-        <div class="card mb-4">
-            <div class="card card-header-actions">
-                <div class="card-header">
-                    Maklumat NOC
-                    <i class="text-muted" data-feather="info" data-bs-toggle="tooltip" data-bs-placement="left"
-                        title="Maklumat NOC"></i>
-                </div>
-                <div class="card-body">
-                    <!-- Form Group (first name)-->
-                    <div class="row">
-                        <div class="py-0">
-                            <label class="small" for="inputTajuk">{{ $noc->nama_jabatan }}
-                                ({{ $noc->sgktn_jabatan }})</label>
-                            <h3 class="my-0" style="text-transform:uppercase;">{{ $noc->tajuk_permohonan }}</h3>
-                            <label class="small" for="noc_id">ID: {{ $noc->noc_id }}{{ $noc->id }}</label>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div>
-                            <label class="small me-2"> {{ $noc->nama_bhgn }} ({{ $noc->sgktn_bhgn }})</label>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                         <div>
-                            <label class="small me-2"> {{ $noc->kod }} - {{ $noc->nama_kat }}</label>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="kodMyProjek">Kod MyProjek</label>
-                                <h5>{{ $noc->kod_myprojek }}</h5>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="noRujukan">No. Rujukan Surat</label>
-                                <h5>{{ $noc->no_rujukan }}</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="datePermohonan">Tarikh Permohonan</label>
-                                @if ($noc->tarikh_permohonan != null)
-                                    <h5>{{ \Carbon\Carbon::parse($noc->tarikh_permohonan)->format('d-m-Y') }}</h5>
-                                @else
-                                    <h5>Tiada data</h5>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="dateSurat">Tarikh Surat Permohonan</label>
-                                @if ($noc->tarikh_permohonan != null)
-                                    <h5>{{ \Carbon\Carbon::parse($noc->tarikh_surat_kementerian)->format('d-m-Y') }}
-                                    </h5>
-                                @else
-                                    <h5>Tiada data</h5>
-                                @endif
-
-                            </div>
-
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="datePermohonan">Tarikh Surat Kelulusan</label>
-                                @if ($noc->tarikh_hantar_surat_lulus != null)
-                                    <h5>{{ \Carbon\Carbon::parse($noc->tarikh_hantar_surat_lulus)->format('d-m-Y') }}
-                                    </h5>
-                                @else
-                                    <h5>Tiada data</h5>
-                                @endif
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mb-3">
-                                <label class="small mb-1" for="datePermohonan">No. Rujukan Surat Kelulusan</label>
-                                @if ($noc->no_rujukan_surat_kelulusan != null)
-                                    <h5>{{ $noc->no_rujukan_surat_kelulusan }}
-                                    </h5>
-                                @else
-                                    <h5>Tiada data</h5>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row">
-                        <div class="mb-3">
-                            <table style="text-align: left">
-                                <tr>
-                                    <td style="width: 7rem">Status NOC :</td>
-                                    <td><span class="badge bg-info text-dark">{!! $noc->nama_status1 !!}</span></td>
-                                </tr>
-                                <tr>
-                                    <td style="width: 7rem"></td>
-                                    <td><span class="badge bg-info text-dark"
-                                            @if ($noc->tarikh_hantar_surat_lulus != null) hidden @endif>{!! $noc->nama_status2 !!}</span>
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </div>
-                    <hr>
-
-                </div>
-            </div>
+    <div class="grid-container">
+        <div class="header">
+            Test Header
+        </div>
+        <div class="main">
+            test main 1
+        </div>
+        <div class="main2">
+            test main 2
         </div>
     </div>
-    <div class="page-break"></div>
-    <div class="col-lg-4">
-        {{-- @include('page.noc.import.status_noc') --}}
-        @include('page.noc.import.status_noc_log')
-    </div>
-
-</div>
 @endsection
 
 @section('js')
